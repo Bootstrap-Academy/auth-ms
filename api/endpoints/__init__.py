1559c58bb058ac761eeb7ebaab342eca79ce5be9
Defelo <elodef42@gmail.com>
2022-09-08 12:33:40 +0200

    Remove test endpoints

--- api/endpoints/__init__.py
+++ api/endpoints/__init__.py
@@ -1,8 +1,8 @@
 from fastapi import APIRouter
 
-from . import oauth, recaptcha, session, test, user
+from . import oauth, recaptcha, session, user
 
 
 ROUTERS: dict[str, tuple[APIRouter, str | None]] = {
-    module.router.tags[0]: (module.router, module.__doc__) for module in [user, session, oauth, recaptcha, test]
+    module.router.tags[0]: (module.router, module.__doc__) for module in [user, session, oauth, recaptcha]
 }
